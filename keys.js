864a20d2d8e9485d52ff35d1417d7df0024a5aea
Meredith Barnett <meredithbarnettcontact@gmail.com>
2020-02-21 12:03:06 -0700

    fixed errors, all functions working

--- keys.js
+++ keys.js
@@ -1,5 +1,7 @@
 console.log('this is loaded');
 
+
 exports.spotify = {
-  id: process.env.89c48e5413294b6f930a26ffeb9be23e
-  secret: process.env.59d61bb9ce774449b02ae78c4beaad48
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -4,63 +4,128 @@ var axios = require("axios");
 
 var keys = require("./keys.js");
 
+var Spotify = require('node-spotify-api');
+
 var spotify = new Spotify(keys.spotify);
 
-// bands in town grab ---------------------------------------
-function artistGrab() {
+var fs = require('fs')
 
 
-    var artist = process.argv[4];
+var caseData = process.argv[2];
 
-    if (process.argv[3] === "concert") {
+var functionData = process.argv[3];
 
 
-        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-            function (response) {
+function pickOne (caseData, functionData) {
+    
+    switch (caseData) {
+        case "song":
+            songGet(functionData);
+            break;
 
-                console.log(data);
-            }
-        );
+        case "movie":
+            movieGrab(functionData)
+            break;
+
+        case "concert":
+            artistGrab(functionData)
+            break;
+        default:
+            console.log("Input invalid, please type node filename song/movie/concert input")
     }
+}
+
+
+pickOne(caseData, functionData);
+
+function artistGrab(artist) {
+
+
+    if (artist === undefined) {
+        artist = "Disturbed"
+    }
+
+    //create a variable that holds your api url 
+
+    var APILink = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    //create you axios call 
+    axios.get(APILink).then(
+        function (response) {
+            console.log(response.data[0]);
+               
+        }
+    );
+
+
 };
 
-artistGrab();
-
-// spotify grab ---------------------------------------------------------
-function songGrab() {
-
-    var Spotify = require('node-spotify-api');
-
-    var track = process.argv[4];
-
-    if (process.arg[3] === "song") {
 
 
-        spotify.search({ type: 'track', query: track }, function (err, data) {
+
+
+function songGet (songName) {
+    if (songName === undefined) {
+        songName = "I want it that way"
+    }
+
+    spotify.search({
+
+        type: "track",
+        query: caseData
+
+    },
+        function (err, data) {
             if (err) {
-                return console.log('Error occurred: ' + err);
+                console.log(err + "has happened ")
             }
+            var holdSong = data.tracks.items;
 
-            console.log(data);
+            for (var i = 0; i < holdSong.length; i++) {
+                console.log(i);
+                // console.log("artist(s): " + holdSong[i].artists.map(getArtists));
+                console.log("song name: " + holdSong[i].name);
+                console.log("preview song: " + holdSong[i].preview_url);
+                console.log("album: " + holdSong[i].album.name);
+                
+                console.log("-----------------------------------");
+
+            }
         });
-    }
 };
 
-songGrab();
 
-// movie grab -------------------------------------------------------------
-function movieGrab() {
-
-
-    var movie = process.argv[4];
-
-    if (process.argv[3] === "film") {
-
-        axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
-            function (response) {
-                console.log(data);
-            })
+function movieGrab(movieName) {
+    if (movieName === undefined) {
+        movieName = "Cinderella"
     }
-};
 
-movieGrab();
+    //create a variable that holds your api url 
+
+    var APILink = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
+
+    //create you axios call 
+    axios.get(APILink).then(
+        function (response) {
+            // console.log(response.data);
+            var JSONData = response.data;
+            console.log("-----------------------------------");
+            console.log("Title: " + JSONData.Title);
+            console.log("-----------------------------------");
+            console.log("Released: " + JSONData.Year);
+            console.log("-----------------------------------");
+            console.log("Rated: " + JSONData.Rated);
+            console.log("-----------------------------------");
+            console.log("Director: " + JSONData.Director);
+            console.log("-----------------------------------");
+            console.log("Actors: " + JSONData.Actors);
+            console.log("-----------------------------------");
+            console.log("Plot: " + JSONData.Plot);
+            console.log("-----------------------------------");
+        });
+}
+
+
+
+
+
